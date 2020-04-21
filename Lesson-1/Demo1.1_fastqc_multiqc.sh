192c83cb85e7c628ff1d5a8d81db1945f737acb3
R. Eric Collins <rec3141@gmail.com>
2020-04-21 15:34:06 -0500

    Update Demo1.1_fastqc_multiqc.sh

--- Lesson-1/Demo1.1_fastqc_multiqc.sh
+++ Lesson-1/Demo1.1_fastqc_multiqc.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 cd data
 
+# run a loop to gunzip all fastq.gz files
 for file in *.fastq.gz
 do
 gunzip ${file}
 done
 
+# make a directory for the output
 mkdir 00_FastQC
 
+# run a loop to run fastqc on all fastq files
 for file in *.fastq;
 do
 fastqc ${file} -o 00_FastQC/
 done
 
+# run multiqc on all fastqc output files
 multiqc .
