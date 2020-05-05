c614991fd6e87940b80e771fcf06c7ef9ab1d6c5
R. Eric Collins <rec3141@gmail.com>
2020-05-05 17:54:16 -0500

    Update Demo5.1_spades.sh

--- Lesson-5/Demo5.1_spades.sh
+++ Lesson-5/Demo5.1_spades.sh
@@ -1,11 +1,19 @@
 cd data
 
-for prefix in `ls *_1.fastq.zip | cut -f1 -d'_' | sort -u`; do 
+# unzip files, concatenate R1's and R2's, and rezip
+for file in *.fastq.zip; do unzip ${file}; done
+cat SRR5780888_1.fastq SRR5780889_1.fastq | gzip > dvh_1.fastq.gz
+cat SRR5780888_2.fastq SRR5780889_2.fastq | gzip > dvh_2.fastq.gz
+
+# rezip individual files
+for file in SRR*.fastq; do gzip ${file}; done
+
+for prefix in `ls *_1.fastq.gz | cut -f1 -d'_' | sort -u`; do 
 
   echo ${prefix}
 
-  R1=( ${prefix}*_1.fastq.zip )
-  R2=( ${prefix}*_2.fastq.zip )
+  R1=( ${prefix}*_1.fastq.gz )
+  R2=( ${prefix}*_2.fastq.gz )
     
   #Trim adapters
   # 'ordered' means to maintain the input order as produced by clumpify.sh
@@ -18,13 +26,13 @@ for prefix in `ls *_1.fastq.zip | cut -f1 -d'_' | sort -u`; do
   #Quality-trim and entropy filter the remaining reads.
   # 'entropy' means to filter out reads with low complexity
   # 'maq' is 'mininum average quality' to filter out overall poor reads
-  bbduk.sh in=filtered.fq.gz out=qtrimmed_${prefix}.fq.gz qtrim=r trimq=10 minlen=70 ordered maxns=0 maq=8 entropy=.95 ow=t
+  bbduk.sh in=filtered.fq.gz out=${prefix}_qtrimmed.fq.gz qtrim=r trimq=10 minlen=70 ordered maxns=0 maq=8 entropy=.95 ow=t
   
-  # Assembly using SPAdes
-  spades.py -o $prefix --12 qtrimmed_${prefix}.fq.gz --only-assembler  --checkpoints all
+  # Assembly quality-trimmed reads using SPAdes
+  spades.py -o ${prefix}_spades --12 ${prefix}_qtrimmed.fq.gz --only-assembler  --checkpoints all
   
   # calculate assembly statistics
-  statswrapper.sh ${prefix}/*.fasta > ${prefix}_stats.txt
+  statswrapper.sh ${prefix}_spades/*.fasta > ${prefix}_stats.txt
   
   cat ${prefix}_stats.txt
   
